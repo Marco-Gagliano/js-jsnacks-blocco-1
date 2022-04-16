2ab36f85e403ca4bd290ada20148c0fa5ba59202
Marco-Gagliano <brazil_94@hotmail.it>
2022-04-16 17:45:20 +0200

    completamento-100%

--- jsnack4/script.js
+++ jsnack4/script.js
@@ -14,30 +14,32 @@
 
 alert ("Benvenuto alla festa a tema 'Il Grande Gatsby'. Prego, inserisca le sue generalità per verificare se è nella lista")
 
+
 const listParty = [
-'Mihai Badia',
-'Riccardo Becattini',
-'Manuel Benini',
-'Giancarlo Croce',
-'Silvia Dugato',
-'Carlo Duminuco Del Monte',
-'Francesco Ercoli',
-'Marco Gagliano',
-'Cristina Gasperini',
-'Chiara Grillo',
-'Manuela Ienuso',
-'Michele Iliescu',
-'Renato Lami',
-'Davide Manciucca',
-'Salvatore Olivieri',
-'Leonardo Pellegrini',
-'Agostino Piquè',
-'Davide Pizzolla',
-'Antonio Pollo',
-'Ivan Russo',
-'Amanjit Singh',
-'Sebastiano Visco'
+  'Mihai Badia',
+  'Riccardo Becattini',
+  'Manuel Benini',
+  'Giancarlo Croce',
+  'Silvia Dugato',
+  'Carlo Duminuco Del Monte',
+  'Francesco Ercoli',
+  'Marco Gagliano',
+  'Cristina Gasperini',
+  'Chiara Grillo',
+  'Manuela Ienuso',
+  'Michele Iliescu',
+  'Renato Lami',
+  'Davide Manciucca',
+  'Salvatore Olivieri',
+  'Leonardo Pellegrini',
+  'Agostino Piquè',
+  'Davide Pizzolla',
+  'Antonio Pollo',
+  'Ivan Russo',
+  'Amanjit Singh',
+  'Sebastiano Visco'
 ];
+
 const listChecked = document.getElementById('checklist');
 const listPartyChecked = document.getElementById('list-checked');
 const listPartyDenied = document.getElementById('list-denied');
@@ -49,8 +51,10 @@ const userList = document.getElementById ('written-list').value;
 let register = false;
 
 for (let i = 0; i < listParty.length; i++){
-  if(listParty[i] === userList);
-  register = true
+
+  if(listParty[i] === userList){
+  register = true;
+  }
 }
 
 if(register) {
@@ -60,14 +64,13 @@ if(register) {
   listPartyDenied.innerHTML = ' ';
   }
   
-
 else {
   listPartyDenied.innerHTML = "Purtroppo non sei sulla lista. La prossima volta prenotati. Sarà per la prossima volta. Ti Aspettiamo";
   listPartyDenied.classList.add('denied');
   listPartyChecked.classList.remove('checked');
   listPartyChecked.innerHTML = ' ';
   }
-})
+});
 
 
 
